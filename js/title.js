9ac66ef00213b7e849137e70fc9030afeb8219d9
onn <owenisworking@gmail.com>
2020-07-16 16:45:42 +0800

    add js/ file

---
+++ js/title.js
@@ -0,0 +1,10 @@
+var d;
+document.addEventListener("visibilitychange", function() {
+    document.hidden ? (clearTimeout(d),
+        d = setTimeout(function() {
+            document.title = "|\uff65\u03c9\uff65\uff40)\u4f60\u770b\u4e0d\u89c1\u6211\u2026\u2026\u4f60\u770b\u4e0d\u89c1\u6211\u2026\u2026\u4f60\u770b\u4e0d\u89c1\u6211\u2026\u2026"
+        }, 500)) : (document.title = "_(:3\u300d\u300d\u8fd8\u662f\u88ab\u53d1\u73b0\u4e86",
+        d = setTimeout(function() {
+        document.title = OriginTitile}, 2E3))
+    }
+console.log('script'); 
